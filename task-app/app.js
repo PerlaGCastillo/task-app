c4aa42e15a0ff76f8f5c3c4e492fef391947f864
Perla Godinez <contactoperlagodinez@gmail.com>
2023-09-07 12:58:04 -0600

    wrks

---
+++ task-app/app.js
@@ -0,0 +1,9 @@
+$(function(){
+    console.log('works');
+
+    $('#search').keyup(funtcion(e) {
+        let search = $('#search').val();
+        console.log(search);
+    })
+
+});
---
+++ task-app/db.php
@@ -0,0 +1,28 @@
+<?php
+
+class Connection{
+    
+    public $host = 'localhost';
+    public $dbname = 'postgres';
+    public $port = '5432';
+    public $user = 'postgres';
+    public $password = 'postgres';
+    public $driver = 'pgslql';
+    public $connect;
+
+    public static function getConnection() 
+    {
+        try{
+            $connection = new Connection();
+            $connection-> connect = new PDO("{$connection->driver}:host={$connection->host};port={
+                $connection->port};dbname={$connection->dbname}", $connection->user, $connection->password);
+                $connection->connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $connection->connect;
+        }catch(PDOException $e){
+            echo "Error: " . $e->getMessage();
+        }
+    }
+}
+Connection::getConnection();
+
+?>
